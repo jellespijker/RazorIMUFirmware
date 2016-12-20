d99cd27ee7bf8d8ef4dcf421ba595794f3f0bf07
Jelle Spijker <spijker.jelle@gmail.com>
2016-12-20 22:13:08 +0100

    Baudrate change

--- Types.h
+++ Types.h
@@ -7,7 +7,7 @@
 #define ACCEL_ADDRESS ((int16_t) 0x53) // 0x53 = 0xA6 / 2
 #define MAGN_ADDRESS  ((int16_t) 0x1E) // 0x1E = 0x3C / 2
 #define GYRO_ADDRESS  ((int16_t) 0x68) // 0x68 = 0xD0 / 2
-#define OUTPUT__BAUD_RATE 115200
+#define OUTPUT__BAUD_RATE 57600
 #define STATUS_LED_PIN 13
 
 #define byte uint8_t
