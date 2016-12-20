8f5612d0677e46a9ee86aaac6c59ad0bc0b60fe6
Jelle Spijker <spijker.jelle@gmail.com>
2016-12-20 22:11:29 +0100

    Update CMake

--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -70,7 +70,6 @@ find_path(ARDUINO_SDK_PATH
           NAMES lib/version.txt
           PATH_SUFFIXES share/arduino
                         Arduino.app/Contents/Resources/Java/
-                        Arduino.app/Contents/Java/
                         ${ARDUINO_PATHS}
           HINTS ${SDK_PATH_HINTS}
           DOC "Arduino SDK path.")
@@ -81,9 +80,4 @@ if(ARDUINO_SDK_PATH)
 else()
     message(FATAL_ERROR "Could not find Arduino SDK (set ARDUINO_SDK_PATH)!")
 endif()
-
-set(ARDUINO_CPUMENU)
-if(ARDUINO_CPU)
-    set(ARDUINO_CPUMENU ".menu.cpu.${ARDUINO_CPU}")
-endif(ARDUINO_CPU)
 
